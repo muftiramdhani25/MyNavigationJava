16248f0f26017d89a748d286a6e0ca50f503c93d
muhammad mufti ramdhani <muftiramdhani25@gmail.com>
2020-03-08 22:03:57 +0700

    Initial commit

---
+++ app/src/main/java/net/growdev/mynavigationjava/ProfileActivity.java
@@ -0,0 +1,14 @@
+package net.growdev.mynavigationjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
